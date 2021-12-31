c93c4e270f8831fc10602c2f2da61c7414f5d8ee
james1112 <79844149+jamesjiwon@users.noreply.github.com>
2021-12-31 13:18:20 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+const dayEl = document.getElementById('day')
+const hoursEl = document.getElementById('hours')
+const minsEl = document.getElementById('mins')
+const secondsEl = document.getElementById('seconds')
+
+function countDown(){
+    const newYear = new Date(2022, 0, 1)
+    const current = new Date()
+
+    const diffTime = Math.abs(newYear-current)
+
+    const days = Math.floor( diffTime/(1000*3600*24) )
+    const hours = Math.floor( diffTime/(1000*3600) %24)
+    const mins = Math.floor( diffTime/(1000*60) %60)
+    const seconds = Math.floor( diffTime/(1000) %60)
+
+    dayEl.innerHTML = formatTime(days)
+    hoursEl.innerHTML = formatTime(hours)
+    minsEl.innerHTML = formatTime(mins)
+    secondsEl.innerHTML = formatTime(seconds)
+}
+function formatTime(time){
+    return time < 10 ? (`0${time}`) : (time);
+}
+countDown()
+
+setInterval(countDown, 1000)
