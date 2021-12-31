a0a0bcbf52a7c7a47988df7f7626dc3265b1e7ff
james1112 <jiwon1112kim@naver.com>
2021-12-31 19:13:30 +0900

    create

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const dayEl = document.getElementById('day')
 const hoursEl = document.getElementById('hours')
 const minsEl = document.getElementById('mins')
 const secondsEl = document.getElementById('seconds')
-const bigEl = document.getElementById('big_countdown')
-
+const bigEl = document.getElementById('iframe')
+const divEl = document.getElementById('b')
+const iframeEl = document.getElementById("iframe")
 function countDown(){
     const newYear = new Date(2022, 0, 1)
     const current = new Date()
@@ -14,7 +15,7 @@ function countDown(){
     const hours = Math.floor( diffTime/(1000*3600) %24)
     const mins = Math.floor( diffTime/(1000*60) %60)
     const seconds = Math.floor( diffTime/(1000) %60)
-    const shows = false
+    shows = 'false'
     dayEl.innerHTML = formatTime(days)
     hoursEl.innerHTML = formatTime(hours)
     minsEl.innerHTML = formatTime(mins)
@@ -27,15 +28,18 @@ function countDown(){
                     hoursEl.style.display = "none";
                     minsEl.style.display = "none";
                     secondsEl.style.display = "none";
-                    bigEl.style.display = " ";
-                    shows = true;
+                    divEl.style.display = "none";
+                    iframeEl.style.display = "block";
+
                 }
             }
         }
     }
-    if (shows) {
-        bigEl.innerHTML = formatTime(seconds)
+    if (seconds == 3) {
+        iframeEl.style.display = "none";
     }
+
+    bigEl.innerHTML = formatTime(seconds)
 }
 function formatTime(time){
     return time < 10 ? (`0${time}`) : (time);
